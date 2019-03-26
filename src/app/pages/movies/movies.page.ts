8c96a8f4f585c79e93d7cc164660200d4ad9613a
A0Fz <caiocbrandao@gmail.com>
2019-03-25 21:37:04 -0300

    moviesApp

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.page.html',
   styleUrls: ['./movies.page.scss'],
+  
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  movies =  [];
+  
+  constructor(private mDBservice: MovieService) { }
 
   ngOnInit() {
+    this.topRatedMovies()
+  }
+
+  topRatedMovies(){
+    this.mDBservice.getTopRatedMovies().subscribe(
+      data=>{
+        //pega a resposta
+        let resposta = (data as any)._body;
+        // converte obj para JSON
+        resposta = JSON.parse(resposta);
+        //console.log(resposta);
+        this.movies = resposta;
+      },
+      error=>{
+        console.log(error);
+      },
+    ).add();
   }
 
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  private URL_API:string = "https://api.themoviedb.org/3";
+  private API_KEY:string = "bc7082ab36eafdab13e91155d6c931d5";
+  constructor(private http : Http) { }
+
+  //retornar lista de top rated movies
+  getTopRatedMovies(){
+    //retorna o resultado baseado na URL da requisiçao
+    return this.http.get(`${this.URL_API}/movie/top_rated?api_key=${this.API_KEY}`)
+  }
 }
